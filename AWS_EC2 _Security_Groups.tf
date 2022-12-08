f1fef5e6c78f16bc6ac050f2c03055d2bc85d369
devksom <96706383+devksom@users.noreply.github.com>
2022-12-08 04:53:22 +0000

    Create AWS_EC2 _Security_Groups.tf

---
+++ AWS_EC2 _Security_Groups.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "bs_websg" {
+  name = "bswebsecuritygroup"
+
+  #Security group inbound rule that allow HTTP traffic
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #Open to anyone
+  }
+  
+
+  #Outgoing traffic
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web1" {
+  ami           = "ami-094125af156557ca2" #us-west2
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  security_groups = ["bswebsecuritygroup"]
+  
+  
+    
+  tags = {
+    web = "web-1"
+  }
+  user_data = <<EOF
+  #!/bin/bash
+  sudo yum update
+  sudo yum install httpd -y
+  sudo service httpd start
+  echo “I made it! This is is awesome!” > /var/www/html/index.html
+EOF
+
+}
+
+
+
