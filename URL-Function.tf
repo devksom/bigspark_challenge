a4771fee3224613675bd9441074d4c5eb69a4e99
devksom <96706383+devksom@users.noreply.github.com>
2022-12-12 12:44:59 +0000

    Update URL-Function.tf

--- URL-Function.tf
+++ URL-Function.tf
@@ -37,3 +37,13 @@ resource "aws_lambda_function_url" "test_latest" {
   function_name      = aws_lambda_function.lambda_function.function_name
   authorization_type = "NONE"
 }
+
+#### Python Code to return a simple message when a you enter an URL in a browser #####
+## Create a new file and name it lambda_function.py. Zip it and place it in the same directory as this file####
+#def lambda_handler(event, context):
+#    # Print the event data
+#    print(event)
+    
+#    # Return a simple message
+#    return "Hello, this is a basic AWS Lambda function!"
+
