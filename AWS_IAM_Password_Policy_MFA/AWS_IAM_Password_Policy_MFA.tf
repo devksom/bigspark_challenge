cf68efe24bac61f550e7cdc9828f58bcaf4126fd
devksom <96706383+devksom@users.noreply.github.com>
2022-12-21 07:28:21 +0000

    Added MFA Code

--- AWS_IAM_Password_Policy_MFA/AWS_IAM_Password_Policy_MFA.tf
+++ AWS_IAM_Password_Policy_MFA/AWS_IAM_Password_Policy_MFA.tf
@@ -3,6 +3,15 @@ resource "aws_iam_account_password_policy" "bgsppwdpolicy" {
     require_numbers = true
     password_reuse_prevention=1
 
-    
-  
+     
 }
+# #Enable MFA for the root user
+# Make sure you undertand the implications of uncommenting the code below
+# resource "aws_iam_mfa_device" "root_mfa" {
+#   user      = "root"
+#   serial_number = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/root"
+# }
+
+# output "root_mfa_arn" {
+#   value = aws_iam_mfa_device.root_mfa.arn
+# }
